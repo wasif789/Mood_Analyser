489c23442da325a1fe58873cbbee5c1c8ff18e29
wasif789 <wsffarid1@gmail.com>
2021-11-11 19:59:27 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mood_Analyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to mood analyser problem");
+        }
+    }
+}
